30a677a7bf77aa4e31f033c240e4c187329a3dae
Sig00rd <hamletolog@gmail.com>
2018-10-17 20:15:45 +0200

    Add mechanism for dprompting the user to choose one ending in conflicts

--- conflictresolver/resolver.py
+++ conflictresolver/resolver.py
@@ -3,15 +3,35 @@ def split_writing_and_other_stuff(csv_line):
 
 
 def build_words_dictionary(csv_lines):
-    words = {}
+    words_dictionary = {}
     for line in csv_lines:
         writing, other_stuff = split_writing_and_other_stuff(line)
-        if writing in words:
-            words[writing].append(other_stuff)
+        if writing in words_dictionary:
+            words_dictionary[writing].append(other_stuff)
         else:
-            words[writing] = [other_stuff]
+            words_dictionary[writing] = [other_stuff]
 
-    return words
+    return words_dictionary
+
+
+def remove_excess_endings(words_dictionary):
+    for word in words_dictionary:
+        words_dictionary[word] = handle_endings(words_dictionary[word])
+
+
+def handle_endings(possible_endings):
+    if len(possible_endings) == 1:
+        return possible_endings.pop()
+    else:
+        user_chosen_ending = get_user_to_choose_ending(possible_endings)
+        return user_chosen_ending
+
+
+def get_user_to_choose_ending(possible_endings):
+    for i in range(len(possible_endings)):
+        print("{:2}: {}".format(i + 1, possible_endings[i]))
+    ending_to_save_number = int(input("Podaj numer zakończenia do zostawienia: ")) - 1
+    return possible_endings[ending_to_save_number]
 
 
 field_separator = ";"
@@ -24,8 +44,14 @@ lines = ["虚空;こくう;pusta przestrzeń, puste niebo;",
 
 words = build_words_dictionary(lines)
 
+# for word in words:
+#     print(word)
+#     endings = words[word]
+#     for ending in endings:
+#         print(ending)
+
+remove_excess_endings(words)
+
 for word in words:
     print(word)
-    endings = words[word]
-    for ending in endings:
-        print(ending)
+    print(words[word])
